6e0a62447602724f6e4363b7eee692416b2d2098
Rodrigo <rodrigo@guedesdesouza.com.br>
2020-05-01 01:28:32 -0300

    Add redcarpet dependency as markdown engine

--- rails/src/hackerboard/app/helpers/application_helper.rb
+++ rails/src/hackerboard/app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def markdown(text)
+    html = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(text).html_safe
+    sanitize html,
+      tags: %w[ a strong em ul ol li blockquote pre code ],
+      attributes: %w[ href ]
+  end
 end
